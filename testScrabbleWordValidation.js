b5b0e29d03dad191d7e501810a0a684dc496a9b1
michaelchang <michael.y.chang@yale.edu>
2023-07-31 12:10:03 -0700

    removed other extraneous codes

--- testScrabbleWordValidation.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.readFile("scrabble_2019.json", 'ascii', function(err, data) {
-    if (err) throw err;
-    const words = JSON.parse(data);
-    const scrabbleSet = new Set(words);
-    console.log(scrabbleSet.has('SANGHA'));
-});
